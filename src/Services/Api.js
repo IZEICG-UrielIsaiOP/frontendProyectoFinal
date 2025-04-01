2033499e50ba360fcd8e16c8a657fbc88af54b10
Uriel Isai Ortiz Pérez <uortiz@izeicg.com>
2025-04-01 00:53:53 -0600

    first commit

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const API1 = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: isProduction
+    ? 'https://server1-rate-limit.onrender.com/api'
+    : 'http://localhost:5001/api',
 });
 
 const API2 = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  baseURL: isProduction
+    ? 'https://sevidor2.onrender.com/api'
+    : 'http://localhost:5002/api',
 });
 
 export { API1, API2 };
